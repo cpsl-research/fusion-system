9e0951d52ca12c9d350e60e473ae2e3a756e8923
Astrosike <huluoming123@gmail.com>
2023-11-22 15:03:13 -0500

    update camera_dirver and camera_perception, interacting with webcam and adding box&texts on the processed data

--- fusion_system_drivers/fusion_system_drivers/camera_driver.py
+++ fusion_system_drivers/fusion_system_drivers/camera_driver.py
@@ -1,23 +1,41 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
 
 class CameraDriver(Node):
     def __init__(self):
         super().__init__('camera_driver')
         self.publisher_ = self.create_publisher(Image, 'camera_image', 10)
         self.timer = self.create_timer(0.5, self.timer_callback)
+        self.bridge = CvBridge()
+
+        # initilize the webcam
+        self.cap = cv2.VideoCapture(0)
+        if not self.cap.isOpened():
+            self.get_logger().error('Failed to open camera')
+            exit(1)
 
     def timer_callback(self):
-        msg = Image()
-        # 填充msg的数据
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing image')
+        ret, frame = self.cap.read()
+        if ret:
+            # change opencv to image message
+            msg = self.bridge.cv2_to_imgmsg(frame, "bgr8")
+            self.publisher_.publish(msg)
+            self.get_logger().info('Publishing image')
+        else:
+            self.get_logger().error('Failed to capture image')
+
+    def __del__(self):
+        # release resources
+        self.cap.release()
 
 def main(args=None):
     rclpy.init(args=args)
     camera_driver = CameraDriver()
     rclpy.spin(camera_driver)
+    camera_driver.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
--- fusion_system_perception/fusion_system_perception/camera_perception.py
+++ fusion_system_perception/fusion_system_perception/camera_perception.py
@@ -1,6 +1,8 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
 
 class CameraPerception(Node):
     def __init__(self):
@@ -10,16 +12,30 @@ class CameraPerception(Node):
             'camera_image',
             self.image_callback,
             10)
-        self.subscription  # prevent unused variable warning
+        self.publisher_ = self.create_publisher(Image, 'camera_dets', 10)
+        self.bridge = CvBridge()
 
     def image_callback(self, msg):
-        # 处理接收到的图像消息
-        self.get_logger().info('Received image')
+        # convert ros image to opencv image
+        cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+
+        # add a box and texts in image to test
+        cv2.rectangle(cv_image, (50, 50), (200, 200), (0, 255, 0), 3)
+        cv2.putText(cv_image, 'CPSL_Fusion', (60, 130), cv2.FONT_HERSHEY_SIMPLEX, 
+                    1, (0, 255, 0), 2, cv2.LINE_AA)
+
+        # convert opencv image back to ros image
+        new_msg = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
+
+        # publish image msg 
+        self.publisher_.publish(new_msg)
+        self.get_logger().info('Published processed image')
 
 def main(args=None):
     rclpy.init(args=args)
     camera_perception = CameraPerception()
     rclpy.spin(camera_perception)
+    camera_perception.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
