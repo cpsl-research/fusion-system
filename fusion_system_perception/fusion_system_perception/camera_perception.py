dd8f626b3d2351d1049752bc919ed50e8c3b44cf
Astrosike <huluoming123@gmail.com>
2023-11-29 09:51:18 -0500

    update perception, reading parameters from bringup/config file

--- fusion_system_perception/fusion_system_perception/camera_perception.py
+++ fusion_system_perception/fusion_system_perception/camera_perception.py
@@ -4,20 +4,31 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+import yaml
 
 class CameraPerception(Node):
     def __init__(self):
         super().__init__('camera_perception')
+        self.bridge = CvBridge()
+        self.config = self.read_yaml_config('../fusion_system_bringup/config/perception.yml')
+
+        # Initialize the DetectNet object with parameters from YAML
+        model = self.config['perception_model']['name']
+        threshold = self.config['perception_model']['thresholds']['high']
+        self.net = jetson.inference.detectNet(model, threshold=threshold)
+
         self.subscription = self.create_subscription(
             Image,
             'camera_image',
             self.image_callback,
             10)
         self.publisher_ = self.create_publisher(Image, 'camera_dets', 10)
-        self.bridge = CvBridge()
 
-        # Initialize the DetectNet object
-        self.net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
+
+    def read_yaml_config(self, config_file):
+        with open(config_file, 'r') as f:
+            config = yaml.safe_load(f)
+        return config
 
     def image_callback(self, msg):
         # Convert ROS image to CUDA (use jetson.utils)
